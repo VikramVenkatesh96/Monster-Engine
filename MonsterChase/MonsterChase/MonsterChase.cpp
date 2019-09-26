fdcb9c46ab5f53b3664ec94b7e7463b4677343e4
u1271129 <vikram.venkatesh@utah.edu>
2019-09-25 20:38:51 -0600

    Completed Infinite User Input

--- MonsterChase/MonsterChase/MonsterChase.cpp
+++ MonsterChase/MonsterChase/MonsterChase.cpp
@@ -1,11 +1,15 @@
 #ifdef _DEBUG
 #define _CRTDBG_MAP_ALLOC
+#include "IO.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <crtdbg.h>
 #include <conio.h>
+#include <ctype.h>
+#include <crtdbg.h>
 #else
+#include "IO.h"
 #include <stdlib.h>
+#include <conio.h>
 #include <stdio.h>
 #endif // _DEBUG
 
@@ -14,12 +18,17 @@
 //Function Declarations
 char* MakeSentence();
 char* MakeSentence(const char*[]);
+void CopyStrings(char*, char*, int, int);
 template<class T>
 int len(T*);
 
-int main( int i_argc, char ** i_argl )
+int main(int i_argc, char ** i_argl)
 {
+	//Did this to remove unsused params warning
 	printf("Running with args: %d , %s\n", i_argc, i_argl[0]);
+	
+	char c = 'n';
+	char * pSentence = nullptr;
 
 	const char * strings[] = {
 		"This",
@@ -27,17 +36,35 @@ int main( int i_argc, char ** i_argl )
 		"a",
 		"test",
 		NULL
-	} ;
+	};
 
-	char * pSentence = MakeSentence();
-	//char * pSentence = MakeSentence( strings );
+	//char * pSentence = MakeSentence( strings ); //To be used for const strings like the one above
+	printf("\nEnter your sentence word-by-word, pressing enter at the end of each word.\n End the sentence by giving enter as the input.\n");
+	do{	
+		switch (c) {
+			case 'n': {
+				pSentence = MakeSentence();
 
-	printf( "The Sentence is: %s and its size is: %d ", pSentence, len(pSentence));
+				if (pSentence[0] == '0') {
+					printf("Nothing was Inputed\n");
+				}
+				else {
+					printf("The Sentence is: %s and its size is: %d ", pSentence, len(pSentence));
+				}
+				free(pSentence);
+				break;
+			}
+			case 'y': break;
+			
+			default: {
+				printf("\nEnter a valid choice!\n");
+			}
+		}
+		printf("Do you want to quit?(y/n): ");
+		c = (char)getchar();
+		c =(char)tolower(c);
+	} while (c != 'y');
 
-
-	free( pSentence );
-
-	_getch();
 #if defined(_DEBUG)
 	_CrtDumpMemoryLeaks();
 #endif // _DEBUG
@@ -45,7 +72,61 @@ int main( int i_argc, char ** i_argl )
 
 //Overload for input string
 char* MakeSentence() {
-	
+	//Init Sentence with nullptr
+	char * sentence = nullptr;
+	bool isEmptyString = true;
+	// Infinitely take input and keep storing in sentence untill user ends it.
+	while (true) {
+		char* temp = (char*)malloc(sizeof(char));
+		bool isEmpty = IO::TakeStringInput(temp);
+		printf("\n");
+		//printf("%d", len(temp));
+		if (isEmpty) {
+			free(temp);
+			break;
+		}
+		else
+		{
+			//No reallocs to be used so used CopyString func to copy strings and paste it to temp
+			//first backup copy the strings if sentence is not empty and then copy the new word to sentence by starting from the end index
+			
+			//printf("%d", size);
+			if (!isEmptyString) {
+				int size = len(sentence);
+				char* temp1 = (char*)malloc(sizeof(char) * size);
+				CopyStrings(temp1,sentence,size,0);
+				free(sentence);
+				sentence = (char*)malloc(sizeof(char) * (size + len(temp) + 2));
+				CopyStrings(sentence, temp1, size,0);
+				free(temp1);
+				CopyStrings(sentence, temp, len(temp), size);
+				sentence[size + len(temp)] = ' ';
+				sentence[size + len(temp) + 1] = '\0';
+			}
+			else {
+				sentence = (char*)malloc(sizeof(char) * (len(temp) + 2));
+				CopyStrings(sentence, temp, len(temp), 0);
+				sentence[len(temp)] = ' ';
+				sentence[len(temp) + 1] = '\0';
+			}
+			
+			isEmptyString = false;
+		}
+		free(temp);
+	}
+	//Handle case when nothing is inputed
+	if (isEmptyString) {
+		free(sentence);
+		char* empty = (char*)malloc(sizeof(char));
+		*empty = '0';
+		return empty;
+	}
+	else
+	{
+		int length = len(sentence);
+		sentence[length-1] = '.';
+		return sentence;
+	}	
 }
 
 //default implementation
@@ -57,11 +138,13 @@ char* MakeSentence(const char* strings[]) {
 	}
 	
 	//Allocate that amount of memory
-	char* sentence = (char*) malloc(sizeof(char) * (numOfChars + len(strings) + 1));
+	char* sentence = (char*) malloc(sizeof(char) * (numOfChars + len(strings) + 1));	//(total number of raw characters + number of spaces and period + end of sentence )
+
 	//Create the sentence
 	int charCount = 0;
 	int wordCount = 0;
 	int writeCount = 0;
+	//iterate over the words and store all characters in sentence
 	while (wordCount < len(strings)) {
 		if (charCount < len(strings[wordCount])) {
 			sentence[writeCount] = strings[wordCount][charCount];
@@ -70,16 +153,24 @@ char* MakeSentence(const char* strings[]) {
 		else {
 			wordCount++;
 			charCount = 0;
-			if(writeCount == numOfChars + len(strings) - 1)
+			// Logic to add spaces and period
+			if (writeCount == numOfChars + len(strings) - 1)
 				sentence[writeCount] = '.';
 			else
 				sentence[writeCount] = ' ';
 		}
 		writeCount++;
 	}
+	//End of sentence
 	sentence[writeCount] = '\0';
-	
+
 	return sentence;
+}
+
+void CopyStrings(char* dest, char* src, int length, int startOffset) {
+	for (int i = 0; i < length; ++i) {
+		dest[startOffset + i] = src[i];
+	}
 }
 
 // Function to find length of any type of array or list
